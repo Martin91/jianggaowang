87b2267d08254d26197d1c059613374480ec27ad
Martin <hongzeqin@gmail.com>
2014-08-31 16:35:31 +0800

    user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def new
+    @user = User.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
+  resources :users, only: [:show, :new, :create]
 
   resources :slides, only: [:show]
 
