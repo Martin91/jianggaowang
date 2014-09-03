3edb624a598b2b1f25360145a217b42c0527fb5c
Martin <hongzeqin@gmail.com>
2014-09-03 21:55:09 +0800

    new model Slide

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831075547) do
+ActiveRecord::Schema.define(version: 20140903135417) do
+
+  create_table "slides", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "filename"
+    t.integer  "user_id"
+    t.boolean  "downloadable"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
