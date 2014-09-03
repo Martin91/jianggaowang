de3befb4ea79fc4f6656599b91229de5f94425a8
Martin <hongzeqin@gmail.com>
2014-09-04 07:47:40 +0800

    create new model category

---
+++ app/models/category.rb
@@ -0,0 +1,4 @@
+class Category < ActiveRecord::Base
+  validates :name, presence: true
+  has_many :slides
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903135417) do
+ActiveRecord::Schema.define(version: 20140903234338) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "slides", force: true do |t|
     t.string   "title"
