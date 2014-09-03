8199c972f9cb6adf5ce2b45fd00f074e0cee7a9f
Martin <hongzeqin@gmail.com>
2014-09-03 20:23:45 +0800

    authencate_user! method support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by id: session[:user_id] if session[:user_id]
   end
+
+  def authenticate_user!
+    unless current_user
+      flash[:danger] = "请先登录"
+      redirect_to login_path
+    end
+  end
 end
