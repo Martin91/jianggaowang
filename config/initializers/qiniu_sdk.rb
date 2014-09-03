ba5397d8af816eea5e290ac90f29ad37726e7010
Martin <hongzeqin@gmail.com>
2014-09-03 10:25:43 +0800

    setup qiniu sdk through a initializer

---
+++ config/initializers/qiniu_sdk.rb
@@ -0,0 +1,2 @@
+Qiniu.establish_connection! :access_key => Rails.application.secrets[:qiniu]['access_key'],
+                            :secret_key => Rails.application.secrets[:qiniu]['secret_key']
