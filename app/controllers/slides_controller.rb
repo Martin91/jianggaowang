e703e23b0c49d8da6c15e992b1bb796ec331b7b6
Martin <hongzeqin@gmail.com>
2014-09-03 22:05:44 +0800

    slides#new action, mainly generate up token and specify callback url

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -3,4 +3,19 @@ class SlidesController < ApplicationController
 
   def show
   end
+
+  def new
+    put_policy = Qiniu::Auth::PutPolicy.new Qiniu::Bucket
+    put_policy.callback_url = request.env["HTTP_HOST"] + qiniu_upload_callback_slides_path
+    put_policy.callback_body = [
+      "slide[filename]=$(fname)",
+      "slide[user_id]=#{current_user.id}",
+      "slide[title]=$(x:title)",
+      "slide[description]=$(x:description)",
+      "slide[downloadable]=$(x:downloadable)"
+    ].join('&')
+
+    @uptoken = Qiniu::Auth.generate_uptoken(put_policy)
+    @resource_key = Time.now.to_i  # use timestamp as resource_key
+  end
 end
