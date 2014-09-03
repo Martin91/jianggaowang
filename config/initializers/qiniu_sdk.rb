8967a49961d0e2a982352df6f9e614a33b1174ef
Martin <hongzeqin@gmail.com>
2014-09-03 20:50:17 +0800

    add bucket config

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -1,2 +1,3 @@
 Qiniu.establish_connection! :access_key => Rails.application.secrets[:qiniu]['access_key'],
                             :secret_key => Rails.application.secrets[:qiniu]['secret_key']
+Qiniu::Bucket = Rails.application.secrets[:qiniu]['bucket']
