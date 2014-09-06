ca6d195242eae04c8cfa7f7186393f68108d267f
Martin <hongzeqin@gmail.com>
2014-09-06 16:28:26 +0800

    a more config to qiniu about pipeline

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -1,3 +1,4 @@
 Qiniu.establish_connection! :access_key => Rails.application.secrets[:qiniu]['access_key'],
                             :secret_key => Rails.application.secrets[:qiniu]['secret_key']
 Qiniu::Bucket = Rails.application.secrets[:qiniu]['bucket']
+Qiniu::MPSQueue = Rails.application.secrets[:qiniu]['mps_queue']
