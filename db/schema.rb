ac432a7d306feb9a14a07d66d6fab97426bc4b4d
Martin <hongzeqin@gmail.com>
2014-09-08 17:21:35 +0800

    add a new field bit to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908082158) do
+ActiveRecord::Schema.define(version: 20140908092033) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20140908082158) do
 
   create_table "users", force: true do |t|
     t.string   "name"
+    t.text     "bio"
     t.string   "email"
     t.string   "password_digest"
     t.string   "avatar"
