b218145a7997a55c38c0e633aec3f66fa28281cc
Martin <hongzeqin@gmail.com>
2014-09-08 16:24:45 +0800

    mount uploader avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  mount_uploader :avatar, AvatarUploader
 
   validates :name, :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
