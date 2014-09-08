427110a7c50f388beae1ac678ca76a486303b2e7
Martin <hongzeqin@gmail.com>
2014-09-08 09:47:51 +0800

    add persistent_state field to slides table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906082541) do
+ActiveRecord::Schema.define(version: 20140908014632) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140906082541) do
     t.integer  "category_id"
     t.boolean  "downloadable"
     t.string   "persistent_id"
+    t.string   "persistent_state"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
