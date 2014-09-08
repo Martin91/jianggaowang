9e8a208c93d3d9c4d2bbf5c0061b7fc5a9524704
Martin <hongzeqin@gmail.com>
2014-09-08 16:36:35 +0800

    setup a has_many association from user to slides

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   mount_uploader :avatar, AvatarUploader
 
+  has_many :slides, dependent: :destroy
+
   validates :name, :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, length: { minimum: 8 }
