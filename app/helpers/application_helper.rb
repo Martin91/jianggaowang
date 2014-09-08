9e4874b8cf8dc885f2e57242937b65b554e7f66b
Martin <hongzeqin@gmail.com>
2014-09-08 17:05:21 +0800

    slide can retrieve first or default preview

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def resource_url(filename)
     "http://#{Qiniu::Bucket}.qiniudn.com/#{filename}"
   end
+
+  def slide_first_or_default_preview_url(slide)
+    if slide.previews.any?
+      resource_url slide.previews.first.filename
+    else
+      image_path 'default_preview.gif'
+    end
+  end
 end
