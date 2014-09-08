b8a91f41b762f0046a1573d4988e1655f3dee37c
Martin <hongzeqin@gmail.com>
2014-09-08 18:13:28 +0800

    user can edit his/her profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ActiveRecord::Base
   has_secure_password
   mount_uploader :avatar, AvatarUploader
 
+  attr_accessor :original_password
+
   has_many :slides, dependent: :destroy
 
   validates :name, :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :password, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }, if: -> { changes.include?(:password) }
 
   def friendly_bio
     bio || "这个讲师暂未留下任何自我介绍"
