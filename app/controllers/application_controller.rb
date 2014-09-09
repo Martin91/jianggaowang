900de1d7fe9f100a710c68315520a2276b043c65
Martin <hongzeqin@gmail.com>
2014-09-09 21:05:11 +0800

    optimize 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   def current_user
     @current_user ||= User.find_by id: session[:user_id] if session[:user_id]
   end
@@ -14,5 +16,9 @@ class ApplicationController < ActionController::Base
       flash[:danger] = "请先登录"
       redirect_to login_path
     end
+  end
+
+  def render_404
+    render file: 'public/404.html', layout: false
   end
 end
