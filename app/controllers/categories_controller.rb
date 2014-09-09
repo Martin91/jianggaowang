ce0f94f9fa644c7f9304cef6666ab4a469b0c5e5
Martin <hongzeqin@gmail.com>
2014-09-09 20:56:38 +0800

    retrieve slides for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find params[:id]
+    @category = Category.find(params[:id])
+    @slides = @category.slides.page(params[:page]).per(params[:per_page])
   end
 end
