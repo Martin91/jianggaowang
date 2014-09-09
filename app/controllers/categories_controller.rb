ae4897cb9667e35f6ed22b6e3e55e5d024bca8f5
Martin <hongzeqin@gmail.com>
2014-09-09 22:05:24 +0800

    all and hottest actions for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,16 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @slides = @category.slides.page(params[:page]).per(params[:per_page])
   end
+
+  def all
+    @category = Category.new name: '所有讲稿'
+    @slides = Slide.page(params[:page]).per(params[:per_page])
+    render 'show'
+  end
+
+  def hotest
+    @category = Category.new name: '热门讲稿'
+    @slides = Slide.hotest.page(params[:page])
+    render 'show'
+  end
 end
