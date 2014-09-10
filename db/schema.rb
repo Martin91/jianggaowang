51466b6ea26712fe8f78f5e1d00c828dbb9a46c5
Martin <hongzeqin@gmail.com>
2014-09-10 20:50:19 +0800

    create new table likes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909134015) do
+ActiveRecord::Schema.define(version: 20140910124840) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20140909134015) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "likes", force: true do |t|
+    t.integer  "slide_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "previews", force: true do |t|
     t.integer  "slide_id"
