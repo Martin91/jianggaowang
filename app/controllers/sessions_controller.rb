98b492a03bad3e4a812f363f11b50fab8593f054
Martin <hongzeqin@gmail.com>
2014-09-23 08:57:08 +0800

    update flash keys

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,12 +11,12 @@ class SessionsController < ApplicationController
 
         redirect_to root_path
       else
-        flash[:danger] = "密码错误，请重新输入"
+        flash[:warning] = "密码错误，请重新输入"
         render 'new'
       end
     else
       @user = User.new
-      flash[:danger] = "用户名不存在，请重新确认您的输入"
+      flash[:warning] = "用户名不存在，请重新确认您的输入"
       render 'new'
     end
   end
@@ -24,9 +24,9 @@ class SessionsController < ApplicationController
   def destroy
     if current_user
       session[:user_id] = nil
-      flash[:success] = "您已退出登录"
+      flash[:information] = "您已退出登录"
     else
-      flash[:danger] = '您还没有登录，退出登录操作无效'
+      flash[:warning] = '您还没有登录，退出登录操作无效'
     end
 
     redirect_to :back
