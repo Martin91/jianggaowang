edc4984a7516cf9e7e98b22c2d7afc39b6b3f302
Martin <hongzeqin@gmail.com>
2014-09-24 08:48:17 +0800

    config the application to use 163 mail to send mails

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,14 @@ module Jianggao
     config.i18n.default_locale = :'zh-CN'
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.163.com',
+      port:                 25,
+      user_name:            Rails.application.secrets[:mail]["user_name"],
+      password:             Rails.application.secrets[:mail]["password"],
+      authentication:       'plain'}
+    config.action_mailer.perform_deliveries = true
   end
 end
