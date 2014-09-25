c78e9297b1ddfdda06747b44dc90177e2ee653bd
Martin <hongzeqin@gmail.com>
2014-09-25 21:44:15 +0800

    send delayed email and update flash message

--- app/controllers/user_passwords_controller.rb
+++ app/controllers/user_passwords_controller.rb
@@ -8,7 +8,9 @@ class UserPasswordsController < ApplicationController
 
     if user
       user.generate_reset_password_token
-      flash[:success] = "我们已经收到您重置密码的请求，请注意查收邮件！"
+      UserPasswordMailer.delay.reset_password(user)
+
+      flash[:success] = "重置密码请求成功，我们已经给您发送了用于重置密码的邮件，请注意查收邮件！"
       redirect_to root_path
     else
       flash[:warning] = "用户名不存在，请重新输入"
