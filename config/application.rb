6bb5f8c88fd67c2ee9ea94459b97a7e2bd85a94f
Martin <hongzeqin@gmail.com>
2014-09-30 10:43:35 +0800

    extract smtp configs to a initializer to avoid error when run rake

--- config/application.rb
+++ config/application.rb
@@ -22,13 +22,6 @@ module Jianggao
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.163.com',
-      port:                 25,
-      user_name:            Rails.application.secrets[:mail]["user_name"],
-      password:             Rails.application.secrets[:mail]["password"],
-      authentication:       'plain'}
     config.action_mailer.perform_deliveries = true
   end
 end
