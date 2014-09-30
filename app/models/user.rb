c084062c55e7d5908ad826dd78681d4136de6a9d
Martin <hongzeqin@gmail.com>
2014-09-30 14:34:14 +0800

    validates name and email case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,10 @@ class User < ActiveRecord::Base
   has_many :collections
   has_many :collecting_slides, through: :collections, source: :slide
 
-  validates :name, :email, presence: true, uniqueness: true
+  validates :name, :email, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, length: { minimum: 8 }, if: -> { changes.include?(:password) }
+  validates_uniqueness_of :name, :email, case_sensitive: false
 
   def friendly_bio
     bio || "这个讲师暂未留下任何自我介绍"
