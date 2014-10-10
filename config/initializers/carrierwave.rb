dc4d8999cccdcb42273c763100b87de3346a5d3e
Martin <hongzeqin@gmail.com>
2014-10-10 12:00:53 +0800

    remove ugly rescue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
-bucket = Rails.application.secrets[:qiniu]['bucket'] rescue ENV["qiniu_bucket"]
+bucket = Rails.application.secrets[:qiniu]['bucket']
 
 ::CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = Rails.application.secrets[:qiniu]['access_key'] rescue ENV["qiniu_access_key"]
-  config.qiniu_secret_key    = Rails.application.secrets[:qiniu]['secret_key'] rescue ENV["qiniu_secret_key"]
+  config.qiniu_access_key    = Rails.application.secrets[:qiniu]['access_key']
+  config.qiniu_secret_key    = Rails.application.secrets[:qiniu]['secret_key']
   config.qiniu_bucket        = bucket
   config.qiniu_bucket_domain = "#{bucket}.qiniudn.com"
   config.qiniu_bucket_private= true #default is false
--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -1,8 +1,8 @@
 # rescue is used to compatiable with coding.net
-access_key = Rails.application.secrets[:qiniu]['access_key'] rescue ENV["qiniu_access_key"]
-secret_key = Rails.application.secrets[:qiniu]['secret_key'] rescue ENV["qiniu_secret_key"]
+access_key = Rails.application.secrets[:qiniu]['access_key']
+secret_key = Rails.application.secrets[:qiniu]['secret_key']
 Qiniu.establish_connection! :access_key => access_key,
                             :secret_key => secret_key
-Qiniu::Bucket = Rails.application.secrets[:qiniu]['bucket'] rescue ENV["qiniu_bucket"]
-Qiniu::MPSQueue = Rails.application.secrets[:qiniu]['mps_queue'] rescue ENV["qiniu_mps_queue"]
-Qiniu::NotificationHost = Rails.application.secrets[:host] rescue ENV["notification_host"]
+Qiniu::Bucket = Rails.application.secrets[:qiniu]['bucket']
+Qiniu::MPSQueue = Rails.application.secrets[:qiniu]['mps_queue']
+Qiniu::NotificationHost = Rails.application.secrets[:host]
